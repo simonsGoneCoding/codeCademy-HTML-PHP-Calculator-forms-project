10d95dba70501c21b5401966bfa403d76ad91f49
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-19 18:05:43 +0000

    Finish Front-end forms.

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!-- Apply your understanding of handling HTML forms in PHP to create a calculator. -->
 
 <html>
-<body>
+  <body>
 <!--Your code goes here-->
+    <h2>Addition</h2>
+    <form action="addition.php" method="get">
+      First number: <input type="number" name="add_first"><br>
+      Second number: <input type="number" name="add_second"><br>
+      <button type="submit">ADD</button>
+    </form>
+    
+    <h2>Dividion</h2>
+    <form action="division.php" method="get">
+      First number: <input type="number" name="div_first">
+      Second number: <input type="number" name="div_second">
+      <button type="submit">DIVIDE</button>
+    </form>
 
-<a href="index.php">Reset</a>
-</body>
+    <a href="index.php">Reset</a>
+  </body>
 </html>
